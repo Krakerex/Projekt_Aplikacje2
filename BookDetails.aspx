8b040f33385fc99b291a321ccb97881a5d194843
Krakerex <58233618+Krakerex@users.noreply.github.com>
2023-06-11 22:17:54 +0200

    Wyświetlanie tytułów

---
+++ BookDetails.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BookDetails.aspx.cs" Inherits="Projekt_Aplikacje2.BookDetails" MasterPageFile="~/Site1.Master" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <link href="styles/bookdetails.css" rel="stylesheet" />
+    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:KsiegarniaConnectionString1 %>"
+        SelectCommand="SELECT Tytul, Autor, Opis, Cena, TypProduktuID FROM Ksiazki WHERE Tytul = @Title">
+        <SelectParameters>
+            <asp:QueryStringParameter Name="Title" QueryStringField="title" />
+        </SelectParameters>
+    </asp:SqlDataSource>
+
+    <div class="book-details">
+        <div class="book-image">
+            <img src="images/cover.jpg" alt="Tytuł książki" />
+        </div>
+        <div class="book-info">
+            <h2 class="book-title">
+                <asp:Label ID="TitleLabel" runat="server" Text='<%# Eval("Tytul") %>'></asp:Label>
+            </h2>
+            <div class="book-author">
+                Autor: <asp:Label ID="AuthorLabel" runat="server" Text='<%# Eval("Autor") %>'></asp:Label>
+            </div>
+            <div class="book-description">
+                Opis: <asp:Label ID="DescriptionLabel" runat="server" Text='<%# Eval("Opis") %>'></asp:Label>
+            </div>
+            <div class="book-price">
+                Cena: <asp:Label ID="PriceLabel" runat="server" Text='<%# Eval("Cena", "{0:C}") %>'></asp:Label>
+            </div>
+            <asp:PlaceHolder ID="AddToCartPlaceHolder" runat="server"></asp:PlaceHolder>
+        </div>
+    </div>
+</asp:Content>
---
+++ BookDetails.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Projekt_Aplikacje2
+{
+    public partial class BookDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string bookTitle = Request.QueryString["title"]; // Pobierz wartość parametru z adresu URL
+
+                // Przypisz parametr do SqlDataSource
+                SqlDataSource1.SelectParameters["Title"].DefaultValue = bookTitle;
+
+                // Pobierz dane dla podanego tytułu z SqlDataSource
+                DataView dv = (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+
+                // Sprawdź, czy dane są dostępne
+                if (dv != null && dv.Count > 0)
+                {
+                    DataRowView row = dv[0];
+
+                    // Przypisz wartości do kontrolek
+                    TitleLabel.Text = row["Tytul"].ToString();
+                    AuthorLabel.Text = row["Autor"].ToString();
+                    DescriptionLabel.Text = row["Opis"].ToString();
+                    PriceLabel.Text = string.Format("{0:C}", row["Cena"]);
+
+                    // Sprawdź typ książki i dodaj odpowiednie przyciski
+                    int bookType = Convert.ToInt32(row["TypProduktuID"]);
+
+                    switch (bookType)
+                    {
+                        case 1:
+                            AddToCartPlaceHolder.Controls.Add(new Button { Text = "Dodaj książkę do koszyka", CssClass = "add-to-cart-button" });
+                            AddOtherTypeButtons(2, "Dodaj audiobook do koszyka");
+                            AddOtherTypeButtons(3, "Dodaj ebook do koszyka");
+                            break;
+                        case 2:
+                            AddToCartPlaceHolder.Controls.Add(new Button { Text = "Dodaj audiobook do koszyka", CssClass = "add-to-cart-button" });
+                            AddOtherTypeButtons(1, "Dodaj książkę do koszyka");
+                            AddOtherTypeButtons(3, "Dodaj ebook do koszyka");
+                            break;
+                        case 3:
+                            AddToCartPlaceHolder.Controls.Add(new Button { Text = "Dodaj ebook do koszyka", CssClass = "add-to-cart-button" });
+                            AddOtherTypeButtons(1, "Dodaj książkę do koszyka");
+                            AddOtherTypeButtons(2, "Dodaj audiobook do koszyka");
+                            break;
+                        default:
+                            // Brak obsługi dla innych typów książek
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void AddOtherTypeButtons(int type, string buttonText)
+        {
+            // Sprawdź, czy istnieje inny typ książki o tym samym tytule
+            SqlDataSource1.SelectCommand = "SELECT Tytul, Autor, Opis, Cena FROM Ksiazki WHERE Tytul = @Title AND TypProduktuID = @TypProduktuID";
+            SqlDataSource1.SelectParameters.Clear();
+            SqlDataSource1.SelectParameters.Add("Title", TitleLabel.Text);
+            SqlDataSource1.SelectParameters.Add("TypProduktuID", type.ToString());
+
+            DataView dv = (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+
+            if (dv != null && dv.Count > 0)
+            {
+                // Dodaj przycisk dla danego typu książki
+                if(dv != null && dv.Count > 0)
+{
+                    foreach (DataRowView rowView in dv)
+                    {
+                        Button button = new Button
+                        {
+                            Text = buttonText,
+                            CssClass = "add-to-cart-button"
+                        };
+
+                        // Dodaj guzik wraz z kontenerem
+                        Panel buttonContainer = new Panel();
+                        buttonContainer.Controls.Add(button);
+                        AddToCartPlaceHolder.Controls.Add(buttonContainer);
+                    }
+                }
+            }
+        }
+    }
+}
